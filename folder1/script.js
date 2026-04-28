6a63d14e1f25db1a05baf445f6ba0bf480bd9963
coralsong <songc215@newschool.edu>
2026-04-28 18:12:31 +0000

    commit

--- folder1/script.js
+++
@@ -1,9 +0,0 @@
-const nose = document.getElementById("nosePath");
-
-function stretchNose(length) {
-  const newLength = 120 + length;
-  nose.setAttribute(
-    "d",
-    `M10 50 L${newLength} 50`
-  );
-}
